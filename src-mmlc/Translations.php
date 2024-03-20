c3a29c6135d4deb3cbf866ccbaa92dc2e3b3cbfa
grandeljay <github@grandels.email>
2024-03-20 16:22:27 +0100

    chore(module): update to v0.3.2

--- src-mmlc/Translations.php
+++ src-mmlc/Translations.php
@@ -3,7 +3,7 @@
 /**
  * Translations
  *
- * @version 0.3.1
+ * @version 0.3.2
  * @author  Jay Trees <translations@grandels.email>
  * @link    https://github.com/grandeljay/modified-translator
  */
